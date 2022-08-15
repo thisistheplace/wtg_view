da0939a95a353de24ef2b3bf9b0707133874f657
Ben Cannell <bencannell@bens-air.home>
2022-08-15 14:55:01 +0100

    Added dockerfile to repo

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 """Landing page"""
 from dashboard import app
 
+# get handle on the flask server for Gunicorn access in Docker
+server = app.server
+
 if __name__ == '__main__':
     app.run_server(debug=True)
